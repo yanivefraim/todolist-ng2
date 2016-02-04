1164dab0b6cbe0e19093a6d8230661317cb7888a
Yaniv Efraim <yaniv.efraim@gmail.com>
2016-02-04 11:16:23 +0200

    bind to dom element value ('value')

--- src/app/todo-input.ts
+++ src/app/todo-input.ts
@@ -3,13 +3,14 @@ import { Component, EventEmitter, Output } from 'angular2/core';
 @Component({
   selector: 'todo-input',
   template: `
-    <input #newItem>
+    <input #newItem [value]="currentItem">
     <button (click)="add(newItem.value, $event)">add</button>
   `
 })
 
 export default class TodoInput {
-  //
+
+  currentItem = '123';
   @Output() onAdd = new EventEmitter();
   add(value) {
     this.onAdd.emit(value);
