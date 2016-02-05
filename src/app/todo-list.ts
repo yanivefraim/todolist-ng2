66df8859fc19be63201a8e0154318d63842943ca
Yaniv Efraim <yaniv.efraim@gmail.com>
2016-02-05 11:08:31 +0200

    remove @Inject demonstration

--- src/app/todo-list.ts
+++ src/app/todo-list.ts
@@ -1,4 +1,4 @@
-import { Component, Inject } from 'angular2/core';
+import { Component } from 'angular2/core';
 import {TodoService} from './todo-service';
 
 @Component({
@@ -10,7 +10,7 @@ import {TodoService} from './todo-service';
 
 export class TodoList {
 
-  constructor(@Inject(TodoService) public todoService, @Inject('theAnswer') answer) {
-    console.log(answer);
+  constructor(public todoService: TodoService) {
+
   }
 }
